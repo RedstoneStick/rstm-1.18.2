540ea4f15013ecd412f605229b41379400ae664e
Guwy <onatkutorhan2004@gmail.com>
2022-06-27 14:29:27 +0300

    Added Magnesium mineral And Its ingot, nugget, dust, crushed, raw variants

--- src/main/java/net/guwy/rstm/item/ModItems.java
+++ src/main/java/net/guwy/rstm/item/ModItems.java
@@ -11,8 +11,41 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RsTm.MOD_ID);
 
+
+    // Ingots
+    public static final RegistryObject<Item> MAGNESIUM_INGOT = ITEMS.register("magnesium_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
+
+
+    // Nuggets
+    public static final RegistryObject<Item> MAGNESIUM_NUGGET = ITEMS.register("magnesium_nugget",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
+
+
+    // Dusts
+    public static final RegistryObject<Item> MAGNESIUM_DUST = ITEMS.register("magnesium_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
+
+
+    // Crushed
+    public static final RegistryObject<Item> MAGNESIUM_CRUSHED = ITEMS.register("magnesium_crushed",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
+
+
+    // Raw Ores
+    public static final RegistryObject<Item> MAGNESIUM_RAW = ITEMS.register("magnesium_raw",
+            () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
+
+
+    // Misc Materials
     public static final RegistryObject<Item> SCRAP_METAL = ITEMS.register("scrap_metal",
             () -> new Item(new Item.Properties().tab(ModCreativeModTabs.MATERIALS)));
+
 
 
     public static void register(IEventBus eventBus){
