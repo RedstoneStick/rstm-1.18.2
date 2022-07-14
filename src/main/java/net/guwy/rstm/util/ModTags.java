f439f634915561965f6ce92f2e77bbbe7b160516
Guwy <onatkutorhan2004@gmail.com>
2022-07-14 17:41:27 +0300

    Updated To Minecraft 1.18.2 And Forge 40.1.0

--- src/main/java/net/guwy/rstm/util/ModTags.java
+++ src/main/java/net/guwy/rstm/util/ModTags.java
@@ -4,32 +4,33 @@ import net.guwy.rstm.RsTm;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 
 public class ModTags {
     public static class Blocks {
-        public static final Tags.IOptionalNamedTag<Block> RANDOM_TAG = tag("randomly_tagged_blocks");
+        public static final TagKey<Block> RANDOM_TAG = tag("randomly_tagged_blocks");
 
-        private static Tags.IOptionalNamedTag<Block> tag(String name){
-            return BlockTags.createOptional(new ResourceLocation(RsTm.MOD_ID, name));
+        private static TagKey<Block> tag(String name){
+            return BlockTags.create(new ResourceLocation(RsTm.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Block> forgeTags(String name){
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Block> forgeTags(String name){
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
 
     public static class Items {
 
 
-        private static Tags.IOptionalNamedTag<Item> tag(String name){
-            return ItemTags.createOptional(new ResourceLocation(RsTm.MOD_ID, name));
+        private static TagKey<Item> tag(String name){
+            return ItemTags.create(new ResourceLocation(RsTm.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Item> forgeTags(String name){
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Item> forgeTags(String name){
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
