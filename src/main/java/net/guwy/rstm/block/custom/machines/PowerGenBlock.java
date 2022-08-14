5a575c6e07669dcc707b1cb73144820f16fc76b2
Guwy <onatkutorhan2004@gmail.com>
2022-08-14 12:06:45 +0300

    Added Immersive Engineering to build.gradle Changed Forge Version to 1.18.2-40.1.51

---
+++ src/main/java/net/guwy/rstm/block/custom/machines/PowerGenBlock.java
@@ -0,0 +1,11 @@
+package net.guwy.rstm.block.custom.machines;
+
+import net.minecraft.world.level.block.Block;
+
+public class PowerGenBlock extends Block {
+
+    public PowerGenBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+}
